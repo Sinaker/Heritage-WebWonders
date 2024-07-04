d0efb4d9f6bd6b8d69c6eedc2474b1df3b179447
Kanishk Pandey <135948593+Sinaker@users.noreply.github.com>
2024-07-04 12:13:55 +0530

    Initial Setup

---
+++ app.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const errorController = require("./controllers/errorController");
+
+const app = express();
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+//Express serves these contents as if they were in the root
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(errorController.get404);
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
