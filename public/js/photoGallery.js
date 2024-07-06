69e3c5eab5620f1147812fedf8186f91033a9450
Kanishk Pandey <135948593+Sinaker@users.noreply.github.com>
2024-07-06 21:50:34 +0530

    Add PhotoGallery Page

--- public/js/photoGallery.js
+++ public/js/photoGallery.js
@@ -1,33 +1,43 @@
 const track = document.querySelector("#image-track");
 
-window.onmousedown = e => {
-    track.dataset.mouseDownAt = e.clientX;
-}
+window.onmousedown = (e) => {
+  track.dataset.mouseDownAt = e.clientX;
+};
 
 window.onmouseup = () => {
-    track.dataset.mouseDownAt = '0';
-    track.dataset.prevPercentage = track.dataset.percentage;
-}
+  track.dataset.mouseDownAt = "0";
+  track.dataset.prevPercentage = track.dataset.percentage;
+};
 
-window.onmousemove = e => {
-    if (track.dataset.mouseDownAt === '0') return;
+window.onmousemove = (e) => {
+  if (track.dataset.mouseDownAt === "0") return;
 
-    const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX;
-    const maxDelta = window.innerWidth / 2;
+  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX;
+  const maxDelta = window.innerWidth / 2;
 
-    const percentage = (mouseDelta / maxDelta) * -100;
-    const nextPercentage = parseFloat(track.dataset.prevPercentage || '0') + percentage;
+  const percentage = (mouseDelta / maxDelta) * -100;
+  let nextPercentage =
+    parseFloat(track.dataset.prevPercentage || "0") + percentage;
 
-    track.dataset.percentage = nextPercentage;
+  // Clamp the percentage to the range [-100, 0] for simplicity
+  nextPercentage = Math.max(Math.min(nextPercentage, 0), -100);
 
-    track.animate({
-        transform: `translate(${nextPercentage}%, -50%)`
-    }, { duration: 12000, fill: "forwards" });
+  track.dataset.percentage = nextPercentage;
 
-    const images = track.querySelectorAll(".image");
-    for (const image of images) {
-        image.animate({
-            objectPosition: `${100 + nextPercentage}% center`
-        }, { duration: 1200000, fill: "forwards" });
-    }
-}
+  track.animate(
+    {
+      transform: `translate(${nextPercentage}%, -50%)`,
+    },
+    { duration: 12000, fill: "forwards" }
+  );
+
+  const images = track.querySelectorAll(".image");
+  for (const image of images) {
+    image.animate(
+      {
+        objectPosition: `${100 + nextPercentage}% center`,
+      },
+      { duration: 1200000, fill: "forwards" }
+    );
+  }
+};
