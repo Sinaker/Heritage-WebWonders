f827a52bee64885f3a156ef6fd6f8bfb36c79860
Kanishk Pandey <135948593+Sinaker@users.noreply.github.com>
2024-07-22 17:00:40 +0530

    Add email subscription

--- routes/home.js
+++ routes/home.js
@@ -6,4 +6,6 @@ const homeController = require("../controllers/home");
 
 router.get("/", homeController.getHome);
 
+router.post("/send/subscription", homeController.sendSub);
+
 module.exports = router;
